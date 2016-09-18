880ad578b27f25abb2ba37b1f23469ee7a2c6093
Robert Ignasiak <tori@robert-i.com>
2016-09-18 19:57:19 +0100

    Initial commit

---
+++ tp1/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace tp1.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ tp1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tp1.Startup))]
+namespace tp1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
